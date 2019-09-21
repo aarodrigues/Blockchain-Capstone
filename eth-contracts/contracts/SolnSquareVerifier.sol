d2e303158cba3755a1fc2ac785f6545752118435
alano <alanoacioli@gmail.com>
2019-09-21 15:50:35 -0300

    Generate verifier.sol using zokrates
    fix variables on square.code
    Generate zokrates files
    Add import verifier.sol on ERC721Mintable.sol

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 import './ERC721Mintable.sol';
+import './../../zokrates/code/square/verifier.sol';
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 contract Verifier {
