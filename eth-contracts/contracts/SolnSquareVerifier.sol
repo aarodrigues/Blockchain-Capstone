9268982fd47c970f1dbf7c12e396d3b555604b48
alano <alanoacioli@gmail.com>
2019-09-23 20:19:30 -0300

    Implement AddSolution and mint functions in SolnSquareVerifier.sol

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -29,6 +29,7 @@ struct Solution{
 
 // TODO define an array of the above struct
 Solution[] solution_list;
+uint256 solutionCount = 0;
 
 // TODO define a mapping to store unique solutions submitted
 mapping(bytes32 => Solution) submittedSolutions;
@@ -43,20 +44,38 @@ event AddedSolution(uint256 index, address addr);
 
 
 // TODO Create a function to add the solutions to the array and emit the event
-function AddSolution(Solution storage solution) internal {
-    solution_list.push(solution);
-    bytes32 key = keccak256(abi.encodePacked(solution.index,solution.addr));
-    submittedSolutions[key] = solution;
-    emit AddedSolution(solution.index,solution.addr);
+function AddSolution(uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[2] memory input) internal {
+
+        bytes32 key = keccak256(abi.encodePacked(input[0], input[1]));
+        require(submittedSolutions[key].addr == address(0), "Solution exists already");
+
+        bool verified = zkVerifier.verifyTx(a, b, c, input);
+        require(verified, "Solution could not be verified");
+
+        Solution memory solution;
+        solution.index = solutionCount;
+        solution.addr = msg.sender;
+        submittedSolutions[key] = solution;
+        solution_list.push(solution);
+
+        emit AddedSolution(solutionCount, msg.sender);
+        solutionCount++;
 }
 
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
-// function mint(address to, uint256 tokenId) external {
-//     //submittedSolutions[]
-// }
+function mint(uint a, uint b,address to) external {
+
+    bytes32 key = keccak256(abi.encodePacked(a, b));
+    require(submittedSolutions[key].addr != address(0), "Solution does not exist");
+    require(submittedSolutions[key].addr == msg.sender, "Only solution address can use it to mint a token");
+    super.mint(to, submittedSolutions[key].index);
+}
 
 }
 
