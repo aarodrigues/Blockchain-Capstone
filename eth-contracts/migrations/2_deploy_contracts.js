4b7ccbabeec0e8df3f437ce2703b87781e88469d
alano <alanoacioli@gmail.com>
2019-09-24 10:12:48 -0300

    Remove unnecessary comments

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,13 +1,9 @@
 // migrating the appropriate contracts
 var SquareVerifier = artifacts.require("./Verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-// var ERC721Mintable = artifacts.require("./ERC721Mintable.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(SquareVerifier);
-  // deployer.deploy(SolnSquareVerifier,SquareVerifier.address, "DykrasToken","DT_721");
   deployer.deploy(SquareVerifier).then(function() {
     return deployer.deploy(SolnSquareVerifier, SquareVerifier.address, "DykrasToken","DT_721");
   });
-  // deployer.deploy(ERC721Mintable, "DykrasToken", "DT_721");
 };
